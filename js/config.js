2392fbe94ab6287e3bd1c1e385fa83fbd79d9222
ruchitapatel01 <patelruchita012000>
2021-02-09 12:49:58 +0530

    initial commit

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_wJLsaO9sM', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+		clientId: '5ig8v3q6rptjlluelleui30nhm' //is this used anywhere?
+    },
+};
